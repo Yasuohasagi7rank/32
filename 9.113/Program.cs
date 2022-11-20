60bdec167f3a778942cfe04e98a630e70de2830a
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:23:34 +0300

    Add files via upload

---
+++ 9.113/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace _9._113
+{
+    class Program
+    {
+        static string Metod (string Name)
+        {
+            Console.WriteLine($"Введите {Name}:");
+            string a = "";
+            while (a.Length < 1)     
+                a = Console.ReadLine();
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            StringBuilder b = new StringBuilder(Metod("предложение"));
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (b[i] == 'с')
+                {
+                    b = b.Remove(i, 1);
+                    b = b.Append('_');
+                }
+            }
+            Console.WriteLine(b);
+       
+        }
+    }
+}
